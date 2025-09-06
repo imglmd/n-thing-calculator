c5c5fd1aef08e9ce4b69dc0c9b0daba7ea9dfe01
imglmd <dangdatmini@gmail.com>
2025-09-07 02:01:43 +0300

    landscape and tablet support

--- app/src/main/java/com/kiryha/calculator/CalculatorButton.kt
+++ app/src/main/java/com/kiryha/calculator/CalculatorButton.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +15,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.kiryha.calculator.ui.theme.CalculatorTheme
 
 @Composable
 fun CalculatorButton(
